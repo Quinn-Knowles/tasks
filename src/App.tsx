3403c1ec3cc1f8dc4c167a2a83ab54533c5915d1
Quinn Colette Knowles <quinn.knowles@maine.edu>
2023-09-07 20:22:04 -0400

    hello world

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Quinn Knowles.
+                automatically reload. Quinn Knowles. Hello World
             </p>
         </div>
     );
