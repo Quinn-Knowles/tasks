c28b0aeab8c7c17e0208bdaa734635b8ebaea99e
Quinn Colette Knowles <quinn.knowles@maine.edu>
2023-09-11 18:53:28 -0400

    this

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,53 @@
+/* eslint-disable prettier/prettier */
 import React from "react";
 import "./App.css";
-import { Button } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import soulframe from "./Assets/images/soulframe image.jpg";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UM COS420 with React Hooks and TypeScript
+                <h1>UM COS420 with React Hooks and TypeScript</h1>
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Quinn Knowles.123 Hello World
-            </p>
-            <Button>Click Me</Button>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                background: "blue",
+                                height: "70px",
+                                width: "40px",
+                                display: "inline-block"
+                            }}
+                        ></div>
+                        <ol>
+                            <li>First thing</li>
+                            <li>Another thing</li>
+                            <li>A third item</li>
+                        </ol>
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                background: "blue",
+                                height: "70px",
+                                width: "40px",
+                                display: "inline-block"
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
+            <img
+                height="700"
+                width="1400"
+                src={soulframe}
+                alt="Image from Soulframe Twitter"
+            />
         </div>
     );
 }
