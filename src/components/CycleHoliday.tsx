136d382b9252db10695e2c1c26bc76e25cd79305
Quinn Colette Knowles <Quinn.knowles@maine.edu>
2023-10-21 14:14:53 -0400

    all components working

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,21 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const Holiday_List: string[] = ["🎄", "🎃", "🎉", "🎆", "🦃"];
+    const [Holiday, set_Holiday] = useState<string>(Holiday_List[0]);
+    function Date_change() {
+        // eslint-disable-next-line prettier/prettier
+        set_Holiday(Holiday === "🎉"? "🎆": Holiday === "🎆" ? "🎃" : Holiday === "🎃" ? "🦃" : Holiday === "🦃"? "🎄": "🎉");
+    }
+    function Name_change() {
+        // eslint-disable-next-line prettier/prettier
+        set_Holiday(Holiday === "🎄" ? "🎆": Holiday === "🎆" ? "🎃": Holiday === "🎃" ? "🎉" : Holiday === "🎉" ? "🦃": "🎄");
+    }
+    return (
+        <span>
+            <Button onClick={Date_change}> Change by Year</Button>
+            <Button onClick={Name_change}> Change by Alphabet</Button>
+            <span data-testid="Holiday display"> Holiday: {Holiday}</span>
+        </span>
+    );
 }
